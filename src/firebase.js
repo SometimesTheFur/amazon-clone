2dbd4578fda1a581869761c3c603cc977dd73bef
sometimesthefur <timothymahr@gmail.com>
2020-09-10 09:53:53 -0400

    Initiated BEM Convention

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDYxOlrmTtalaPGR8DvrY5_L-5fqz8n8N0",
+    authDomain: "clone-ea67e.firebaseapp.com",
+    databaseURL: "https://clone-ea67e.firebaseio.com",
+    projectId: "clone-ea67e",
+    storageBucket: "clone-ea67e.appspot.com",
+    messagingSenderId: "776967902972",
+    appId: "1:776967902972:web:4f096c2d7ea6d269a1e979",
+    measurementId: "G-M36NZKKBG6"
+  };
