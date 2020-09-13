bef0613984ad8378f25e656e6c5a4ca0654cf921
sometimesthefur <timothymahr@gmail.com>
2020-09-13 07:47:26 -0400

    cart count

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,8 +3,11 @@ import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import {Link} from 'react-router-dom';
+import { useStateValue } from '../StateProvider';
 
 function Header() {
+    const [{cart}, dispatch] = useStateValue();
+
     return (
         <div className='header'>
             <Link to="/">
@@ -35,7 +38,7 @@ function Header() {
                 <Link to="/checkout">
                     <div className='header__optionBasket'>
                         <ShoppingCartOutlinedIcon />
-                        <span className='header__lineTwo header__basketCount'>0</span>
+                        <span className='header__lineTwo header__basketCount'>{cart?.length}</span>
                     </div>
                 </Link>
                 
