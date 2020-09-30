e5bb62cad2f7482ec035d454389f585baa14c361
sometimesthefur <timothymahr@gmail.com>
2020-09-30 09:57:58 -0400

    Sign Out click stays on Home and logs out

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -25,7 +25,7 @@ function Header() {
             </div>
 
             <div className='header__nav'>
-                <Link to='/login'>
+                <Link to={!user && '/login'}>
                     <div onClick={handleAuthentication} className='header__option'>
                         <span className='header__optionLineOne'>Hello, Guest</span>
                         <span className='header__lineTwo'>{user ? 'Sign Out' : 'Sign In'}</span>
