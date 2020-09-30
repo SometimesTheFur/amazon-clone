e13e952473fe0d2e7820fbb0918513520119b8c5
sometimesthefur <timothymahr@gmail.com>
2020-09-30 14:59:02 -0400

    Styling Payment Page

--- src/Cart/Payment/Payment.js
+++ src/Cart/Payment/Payment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useStateValue } from '../../StateProvider';
 import CheckoutProd from '../Checkout/CheckoutProd';
 import './Payment.css';
@@ -9,6 +10,12 @@ function Payment() {
     return (
         <div className='payment'>
             <div className='payment__container'>
+                <h1>
+                    Checkout (
+                        <Link to="/checkout">{cart?.length} items</Link>
+                    )
+                </h1>
+
                 <div className='payment__section'>
                     <div className='payment__title'>
                         <h3>Delivery Address</h3>
